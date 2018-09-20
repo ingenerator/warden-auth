1d3f2a6c6fa05fd6075c28edb48b0cb11e1c307b
Andrew Coulton <andrew@ingenerator.com>
2018-09-20 16:16:15 +0100

    Add alias for phpunit classes to support 4.x and 6.x

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -9,3 +9,19 @@ $mock_loader->addPsr4('test\\unit\\Ingenerator\\Warden\\Auth\\', [__DIR__.'/unit
 $mock_loader->addPsr4('test\\integration\\Ingenerator\\Warden\\Auth\\', [__DIR__.'/integration/']);
 
 $mock_loader->register();
+
+
+// Workaround to allow use with old and new phpunit
+if ( ! class_exists('\PHPUnit\Framework\MockObject\Stub\ReturnCallback')) {
+    class_alias(
+        'PHPUnit_Framework_MockObject_Stub_ReturnCallback',
+        '\PHPUnit\Framework\MockObject\Stub\ReturnCallback'
+    );
+}
+
+if ( ! class_exists('\PHPUnit\Framework\MockObject\Generator')) {
+    class_alias(
+        'PHPUnit_Framework_MockObject_Generator',
+        '\PHPUnit\Framework\MockObject\Generator'
+    );
+}
